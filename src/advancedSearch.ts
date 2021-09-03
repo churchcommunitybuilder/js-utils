790cf8fa41b010a1fe6c01365e601cd99e284926
sydneyfowler <sydneyfowler@churchcommunitybuilder.com>
2021-09-03 12:20:21 -0600

    CHMS-5290 - sf - add advanced search operator types

--- src/advancedSearch.ts
+++ src/advancedSearch.ts
@@ -1,3 +1,26 @@
+export type ArrayOperator = 'in' | 'not_in' | 'is_set' | 'is_not_set'
+
+export type BooleanOperator = 'is_set' | 'is_not_set'
+
+export type StringOperator =
+  | 'equal'
+  | 'not_equal'
+  | 'is_set'
+  | 'is_not_set'
+  | 'in'
+  | 'not_in'
+  | 'contains'
+  | 'does_not_contain'
+  | 'like'
+  | 'not_like'
+  | 'contains_like'
+  | 'does_not_contain_like'
+  | 'starts_with'
+  | 'ends_with'
+  | 'legacy'
+
+export type AnyOperator = ArrayOperator | BooleanOperator | StringOperator
+
 export interface Constraint {
   id: string
   operator: string
